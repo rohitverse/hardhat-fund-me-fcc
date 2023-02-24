d2a89a1193bf89f3256d1acb6464c45ea222742c
rohitverse <rohitkr23072001@gmail.com>
2023-02-24 18:59:02 +0530

    initial commit

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,8 @@
+
+module.exports = async ({getNamedAccounts, deployments})=>{
+    const {deploy , log}=deployments;
+    const {deployer} = await getNamedAccounts;
+    const chainId= network.config.chainId;
+
+    
+}
