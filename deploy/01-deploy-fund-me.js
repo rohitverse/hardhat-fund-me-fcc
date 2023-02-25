c279673e933ff6d6a14ef89d0e22cb077f91c59a
rohitverse <rohitkr23072001@gmail.com>
2023-02-25 18:42:25 +0530

    second

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -3,6 +3,4 @@ module.exports = async ({getNamedAccounts, deployments})=>{
     const {deploy , log}=deployments;
     const {deployer} = await getNamedAccounts;
     const chainId= network.config.chainId;
-
-    
 }
