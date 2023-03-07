933ad3f2de4108961acd0904ad8f8aa851db7aa0
rohitverse <rohitkr23072001@gmail.com>
2023-03-07 18:14:21 +0530

    ./

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,11 +1,11 @@
 const networkConfig={
     5:{
         name: "goerli",
-        ethUsdPriceFeed: " "
+        ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e"
     },
-    137:{
-        name: "polygon",
-        ethUsdPriceFeed: " "
+    11155111:{
+        name: "sepolia",
+        ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306"
     },
 
 }
